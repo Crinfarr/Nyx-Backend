6a7c8b6312db240b75919f7b4f8e954ce77455cc
Asher Hoeveler <theargon0@gmail.com>
2023-08-01 11:03:09 -0500

    overflow error oops

--- generate-key.sh
+++ generate-key.sh
@@ -1,2 +1,2 @@
 openssl genrsa 2048 > ca-key.pem;
-openssl req -new -x509 -nodes -days 365000 -key ca-key.pem -out ca-cert.pem
+openssl req -new -x509 -nodes -days 36500 -key ca-key.pem -out ca-cert.pem
